0fd2e5b05ea423416e77dd30552005f617c66fb2
andreeacirciu <andreeacirciu@gmail.com>
2020-08-02 15:49:57 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/fasttrackit/onlinebookshop/OnlineBookshopApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlinebookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fasttrackit/onlinebookshop/OnlineBookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlinebookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
